5d04d0331d9f91616cb7b9c82394ef99ba27322b
ThaTeo <mttbettani@gmail.com>
2026-05-16 21:32:03 +0200

    testing last experiment on lower iterations

---
+++ examples/new-experiments/1.5 best mutations for reduced iterations/Fine tuning with random search.jl
@@ -0,0 +1,106 @@
+push!(LOAD_PATH, "../../../src/")
+
+using SpikingNeuralEvolution
+
+using DataFrames
+using Statistics
+using Distributed
+using Random
+
+# Add workers (use all CPU cores)
+if nprocs() == 1
+    addprocs(Sys.CPU_THREADS)
+end
+
+# Distribute code to all workers
+@everywhere begin
+    push!(LOAD_PATH, "../../../src/")
+    using SpikingNeuralEvolution
+    using Statistics
+
+    n = UInt16(5)
+
+    function XOR(inputs::Vector{Bool})
+	    return [reduce(xor, inputs)]
+    end
+end
+
+# convert "i | float float float float float float float float float float uint16" to (float float float float float float float float)
+file = readlines(open("1.4 digest.txt", "r"))
+combinations = [[parse(Float64, element) for element in (Vector{String}(split(split(strip(line), "│")[2]))[1:8])] for line in file]
+
+# Function to run a single evolution experiment
+@everywhere function run_evolution_experiment(params::Union{Vector{Float64}, Vector{Float64}})
+    new_layer, remove_layer, new_neuron, remove_neuron, new_rule, remove_rule, new_input, remove_input = params
+
+    evolution_parameters = EvolutionParameters(
+        UInt32(15),    # How many simulations
+        UInt32(200),    # How many iterations per simulation
+        UInt32(80),    # Min number of random population
+        UInt32(120),   # Max number of random population
+        UInt16(1),     # Min numbe r of random hidden layers
+        UInt16(3),     # Max number of random hidden layers
+        SpikingNeuralEvolution.MaxIterations, # Stopping criteria
+        1.0,    # % of examples
+        1.0     # Threshold
+    )
+
+    mutation_probabilities = MutationProbabilities(
+        new_layer,      # New layer
+        remove_layer,   # Remove layer
+        new_neuron,     # New neuron
+        remove_neuron,  # Remove neuron
+        new_rule,       # Add rule
+        remove_rule,    # Remove rules
+        new_input,      # Add input line
+        remove_input    # Remove input line
+    )
+
+    histories = Evolve(XOR, n, evolution_parameters, mutation_probabilities)
+
+    final_values = []
+    how_many_successful = 0
+
+    for key in keys(histories)
+        push!(final_values, histories[key][2][end])
+        if histories[key][2][end] == 1
+            how_many_successful += 1
+        end
+    end
+
+    return (new_layer, remove_layer, new_neuron, remove_neuron,
+        new_rule, remove_rule, new_input, remove_input,
+        mean(final_values), maximum(final_values), how_many_successful)
+end
+
+# Run experiments in parallel using pmap
+println("Starting fitting with $(nworkers()) workers on previous 1000 parameter combinations...")
+results_list = pmap(run_evolution_experiment, combinations)
+
+# Convert results to DataFrame
+results = DataFrame(
+    NewLayer=Float64[],
+    RemoveLayer=Float64[],
+    NewNeuron=Float64[],
+    RemoveNeuron=Float64[],
+    NewRule=Float64[],
+    RemoveRule=Float64[],
+    NewInput=Float64[],
+    RemoveInput=Float64[],
+    MeanFitness=Float64[],
+    MaxFitness=Float64[],
+    SuccessfulSimulations=UInt16[]
+)
+
+for result in results_list
+    push!(results, result)
+end
+
+sort!(results, :MeanFitness, rev=true)
+
+println("\n=== !200! Iterations Results ===")
+println("Total experiments: $(nrow(results))")
+println("\nTop 1000 results by MeanFitness:")
+println(results[1:min(1000, nrow(results)), :])
+
+exit(0)
